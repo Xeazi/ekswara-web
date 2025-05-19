1873c0d1c2212333d8653ac675f40600ec400188
Xeazi <daffaghosan69@gmail.com>
2025-05-20 00:27:20 +0700

    'Adding header and implement browser router'

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+
 function Header() {
   return (
    <header className="bg-white shadow-lg py-4 w-full h-[88px] flex items-center justify-between px-8">
