49a74e4ba86c6fa3f80cc09aa33145a5713a1005
Xeazi <daffaghosan69@gmail.com>
2025-05-20 00:29:35 +0700

    'Creating header and implement browser router'

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import App from './App.jsx'
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
