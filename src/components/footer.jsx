723a16411acc8cdbdcd453d7460903c8ef1efbd5
Xeazi <daffaghosan69@gmail.com>
2025-05-20 00:08:20 +0700

    create footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div>
+            <footer className="bg-main text-black py-8 h-[100px] min-h-[50px] w-full flex items-center justify-center">
+                <div className="container mx-auto text-center">
+                <p className='text-lg text-text'>Copyright © Ekswara 2025.All Rights Reserved.</p>
+                </div>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
