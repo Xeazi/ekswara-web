d9cd0edd10d870b2c0b637eaa256ee11eaf81c92
Xeazi <daffaghosan69@gmail.com>
2025-05-20 00:37:56 +0700

    making header and footer taking up full width

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
     return (
         <div>
-            <footer className="bg-main text-black py-8 h-[100px] min-h-[50px] w-full flex items-center justify-center">
+            <footer className="bg-main text-text py-8 h-[100px] min-h-[50px] w-full flex items-center justify-center">
                 <div className="container mx-auto text-center">
                 <p className='text-lg text-text'>Copyright © Ekswara 2025.All Rights Reserved.</p>
                 </div>
