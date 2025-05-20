cb57aaf69ac38a0567c591cf84753c1d41002af7
Xeazi <daffaghosan69@gmail.com>
2025-05-20 12:25:59 +0700

    'change color for the hover on Link'

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,9 +6,9 @@ export const Header = () => {
   <header className="bg-white shadow-lg py-4 w-full h-[88px] flex items-center justify-between px-8">
       <div className="text-lg font-bold text-[var(--color-text)]">UrbanRayaJakarta</div>
       <nav className="flex gap-6">
-        <Link to="/" className="text-[var(--color-text)] hover:text-[var(--color-main)]">Home</Link>
-        <Link to="/destination" className="text-[var(--color-text)] hover:text-[var(--color-main)]">Destination</Link>
-        <Link to="/event" className="text-[var(--color-text)] hover:text-[var(--color-main)]">Event</Link>
+        <Link to="/" className="text-[var(--color-text)] hover:text-[var(--color-text-light)]">Home</Link>
+        <Link to="/destination" className="text-[var(--color-text)] hover:text-[var(--color-text-light)]">Destination</Link>
+        <Link to="/event" className="text-[var(--color-text)] hover:text-[var(--color-text-light)]">Event</Link>
       </nav>
     </header>
   )
