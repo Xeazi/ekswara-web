42f052e4f361ba290d7116a67e8adeb29d6a26c4
Xeazi <daffaghosan69@gmail.com>
2025-07-09 21:34:48 +0700

    'Changing the way to use .env'

--- src/pages/TicketReservationPage.jsx
+++ src/pages/TicketReservationPage.jsx
@@ -346,7 +346,6 @@ function ReservationPage() {
         <h1 className="text-4xl font-bold text-center text-text mb-12">
           Ticket Reservation
         </h1>
-
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
           {" "}
           {/* Kolom Kiri: Customer Details */}
